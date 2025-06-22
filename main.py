934c4f9fd8d96d36e7f4a97ac7fcf84bad41dc0b
kitanaya <seb.heinzmann@gmail.com>
2025-06-22 18:20:23 +0200

    Update main.py

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ import os
 from google import genai
 from google.genai import types
 from dotenv import load_dotenv
-# managed to not log in to windows yet
+# managed to not log in to windows yet!!!
 
 
 def main():
