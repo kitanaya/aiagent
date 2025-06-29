9207f715f38bbf11ac2a8bf63292166cd55bc9cc
kitanaya <seb.heinzmann@gmail.com>
2025-06-29 20:05:08 +0200

    Update main.py

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ import os
 from google import genai
 from google.genai import types
 from dotenv import load_dotenv
-# saturday and work almost done
+# raspi and pihole back up and running after years of ignoring it
 
 
 def main():
