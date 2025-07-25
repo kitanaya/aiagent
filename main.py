9e26626a05ec8ac2de32fa95af5a368fb2bd703b
kitanaya <seb.heinzmann@gmail.com>
2025-07-25 05:05:42 +0000

    Update main.py

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ import os
 from google import genai
 from google.genai import types
 from dotenv import load_dotenv
-# doing 100 in ssf? watching python tuts on the side..?
+# lost xp this morning wtf
 
 
 def main():
