0778a5e6afe25af0f68d87547d0ba07bbdbafd06
kitanaya <seb.heinzmann@gmail.com>
2025-08-06 14:48:52 +0200

    Batman3

--- main.py
+++ main.py
@@ -2,24 +2,33 @@ import os
 import sys
 from dotenv import load_dotenv
 from google import genai
+from google.genai import types
 
 
 def main():
     load_dotenv()
+
     args = sys.argv[1:]
 
     if not args:
         print('Usage: uv run main.py "your prompt here"')
         print('\nExample: uv run main.py "Name the top three cyber security jobs."')
         sys.exit(1)
-    user_prompt = " ".join(args)
 
     api_key = os.environ.get("GEMINI_API_KEY")
     client = genai.Client(api_key=api_key)
 
+    user_prompt = " ".join(args)
+    messages = [
+        types.Content(role="user", parts=[types.Part(text=user_prompt)]),
+    ]
+    generate_content(client, messages)
+
+
+def generate_content(client, messages)
     response = client.models.generate_content(
-     model = 'gemini-2.0-flash-001',
-     contents = user_prompt
+        model = 'gemini-2.0-flash-001',
+        contents = messages,
     )
 
     print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
