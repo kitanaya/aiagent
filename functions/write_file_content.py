71027ed36f7498f2378b7dc69733aef34cfe63dc
kitanaya <seb.heinzmann@gmail.com>
2025-08-07 10:31:23 +0200

    update write_file due to boots and instructor solution

--- functions/write_file_content.py
+++ functions/write_file_content.py
@@ -7,14 +7,21 @@ def write_file(working_directory, file_path, content):
     if not abs_file_path.startswith(abs_working_dir):
         return f'Error: Cannot write to "{file_path}" as it is outside the permitted working directory'
     
+    if not os.path.exists(abs_file_path):
+        try:
+            os.makedirs(os.path.dirname(abs_file_path), exist_ok=True)
+        except Exception as e:
+            return f"Error:  creating directory: {e}"
+    
+    if os.path.exists(abs_file_path) and os.path.isdir(abs_file_path):
+        return f'Error: "{file_path}" is a directory, not a file'
+    
     try:
-        dir_name = os.path.dirname(abs_file_path)
-        if not os.path.exists(dir_name):
-            os.makedirs(dir_name)
         with open(abs_file_path, "w") as f:
             f.write(content)
+        return f'Successfully wrote to "{file_path}" ({len(content)} characters written)'
     except Exception as e:
         return f'Error: Writing to file "{file_path}": {e}'
     
-    return f'Successfully wrote to "{file_path}" ({len(content)} characters written)'
+
     
