73e6f7d4ca3feb6eb99e110ed11e24449594de2d
kitanaya <seb.heinzmann@gmail.com>
2025-08-07 09:14:13 +0200

    CH3 L3

--- functions/get_files_info.py
+++ functions/get_files_info.py
@@ -1,4 +1,6 @@
 import os
+from functions.config import MAX_CHARS
+
 
 def get_files_info(working_directory, directory="."):
 	abs_path = os.path.abspath(os.path.join(working_directory, directory))
@@ -19,4 +21,21 @@ def get_files_info(working_directory, directory="."):
 	
 
 def get_file_content(working_directory, file_path):
-	pass
+	abs_working_dir = os.path.abspath(working_directory)
+	target_dir = os.path.abspath(os.path.join(working_directory, file_path))
+	if not target_dir.startswith(abs_working_dir):
+		return f'Error: Cannot read "{file_path}" as it is outside the permitted working directory'
+	if not os.path.isfile(target_dir):
+		return f'Error: File not found or is not a regular file: "{file_path}"'
+	
+	try:
+		with open(target_dir, "r") as f:
+			file_content_string = f.read(MAX_CHARS)
+			if len(file_content_string) < MAX_CHARS:
+				return file_content_string
+			else:
+				return f'{file_content_string}\n[...File "{file_path}" truncated at 10000 characters].'
+	except Exception as e:
+		return f"Error: {str(e)}"
+	 
+
--- tests.py
+++ tests.py
@@ -1,21 +1,39 @@
 from functions.get_files_info import get_files_info
-
-print("Result for current directory:")
-print(get_files_info("calculator", "."))
-
-print("Result for 'pkg' directory:")
-print(get_files_info("calculator", "pkg"))
-
-print("Result for '/bin' directory:")
-print(get_files_info("calculator", "/bin"))
-
-print("Result for '../' directory:")
-print(get_files_info("calculator", "../"))
+from functions.get_files_content import get_file_content
 
 
+def test():
+    #print("Result for current directory:")
+    #print(get_files_info("calculator", "."))
+    #
+    #print("Result for 'pkg' directory:")
+    #print(get_files_info("calculator", "pkg"))
+    #
+    #print("Result for '/bin' directory:")
+    #print(get_files_info("calculator", "/bin"))
+    #
+    #print("Result for '../' directory:")
+    #print(get_files_info("calculator", "../"))
 
+    #result = get_file_content("calculator", "lorem.txt")
+    #print("File content of lorem:")
+    #print(result)
 
+    print("Result for main.py")
+    print(get_file_content("calculator", "main.py"))
+    print("")
 
+    print("Result for pkg/calculator.py")
+    print(get_file_content("calculator", "pkg/calculator.py"))
+    print("")
 
+    print("Result for /bin/cat")
+    print(get_file_content("calculator", "/bin/cat"))
+    print("")
 
+    print("Result for pkg/does_not_exist.py")
+    print(get_file_content("calculator", "does_not_exist.py"))
+    print("")
 
+if __name__ == "__main__":
+    test()
