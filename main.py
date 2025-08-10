1d05eac3cec877d1afad7a032208673f98245d55
kitanaya <seb.heinzmann@gmail.com>
2025-08-10 23:57:39 +0200

    first declaration

--- main.py
+++ main.py
@@ -3,9 +3,9 @@ import sys
 from dotenv import load_dotenv
 from google import genai
 from google.genai import types
-from config import system_prompt
+from prompts import system_prompt
+from available_functions import available_functions
 
-# kekw
 
 def main():
     load_dotenv()
@@ -36,14 +36,20 @@ def generate_content(client, messages, verbose):
     response = client.models.generate_content(
         model = 'gemini-2.0-flash-001',
         contents = messages,
-        config = types.GenerateContentConfig(system_instruction=system_prompt),
+        config = types.GenerateContentConfig(
+            tools=[available_functions],
+            system_instruction=system_prompt),
     )
     if verbose:
         print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
         print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
-        
-    print("Response:")
-    print(response.text)
+
+    if response.function_calls:
+        for function_call_part in response.function_calls:
+            print(f"Calling function: {function_call_part.name}({function_call_part.args})")
+    else:
+        print("Response:")
+        print(response.text)
 
 
 if __name__ == "__main__":
